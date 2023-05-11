47bea95ed3bc7c63efa2ac8fe7a2ca52908a4d92
Firmansyah Yanuar <fyfirman@gmail.com>
2023-05-12 00:07:01 +0700

    feat: form and refactor

--- src/components/Pev.vue
+++ src/components/Pev.vue
@@ -1,23 +1,41 @@
-<script>
+<script lang="ts">
 import { Plan } from "pev2"
 import "pev2/dist/style.css"
+import { ref } from "vue";
 
-const plan = "Seq Scan on foo  (cost=0.00..155.00 rows=10000 width=4)";
+const plan = "Seq Scan on empty_plan  (cost=0.00..155.00 rows=10000 width=4)";
+const error = ref(false)
 
 export default {
   name: "PEV2 example",
+  props: {
+    plan: {
+      type: String,
+      default: plan,
+    }
+  },
   components: {
     pev2: Plan,
   },
-  data() {
+  setup() {
+
     return {
-      plan: plan,
+      error,
     }
+  },
+  errorCaptured(err: any, vm: any, info: any) {
+    error.value = true;
+    console.error(err, vm, info);
+    return false;
   },
 }
 
 </script>
 
 <template>
-  <pev2 :plan-source="plan" plan-query="" />
+  <pev2 v-if="!error" :plan-source="plan" plan-query="" />
+  <div v-else>
+    <h1>PEV2</h1>
+    <p>There was an error loading the plan.</p>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,101 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import PevVue from '../components/Pev.vue';
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
 
 const visible = ref(false);
+const label = ref(`Query Plan ${new Date().toISOString()}`);
+const queryPlan = ref('');
+const query = ref('');
 
-
+const pevFinalData = reactive({
+  plan: "Seq Scan on empty_plan  (cost=0.00..155.00 rows=10000 width=4)",
+  query: '',
+  label: `Query Plan ${new Date().toISOString()}`,
+});
 function toggleVisible() {
   visible.value = !visible.value;
+}
+
+function savePlan(){
+  pevFinalData.plan = queryPlan.value;
+  pevFinalData.query = query.value;
+  pevFinalData.label = label.value ? label.value : `Query Plan ${new Date().toISOString()}`;
+
+  toggleVisible();
 }
 </script>
 
 <template>
   <main>
-    <Button label="Show" @click="toggleVisible" />
-    <PevVue />
+    <div class="header">
+      <h1>
+        {{ pevFinalData.label }}
+      </h1>
+      <Button label="Edit Plan" size="small" class="edit-plan-btn" @click="toggleVisible" />
+    </div>
+    <PevVue :plan="pevFinalData.plan" :query="query"/>
     <Dialog
       v-model:visible="visible"
       modal
       header="Edit Plan"
-      :style="{ width: '80vw' }"
+      :style="{ width: '1024px' }"
     >
-      <p>Edit query modal</p>
+      <span class="d-flex flex-column w-100">
+        <label for="label" class="form-label">Label (Optional)</label>
+        <InputText id="label" v-model="label" class="w-100" />
+      </span>
+      <span class="d-flex flex-column mt-2 w-100">
+        <label class="form-label">Plan (JSON)</label>
+        <Textarea v-model="queryPlan" rows="5" cols="30" class="w-100 query-plan-text-area" />
+      </span>
+      <span class="d-flex flex-column mt-2 w-100">
+        <label class="form-label">Query</label>
+        <Textarea v-model="query" rows="5" cols="30" class="w-100 query-text-area" />
+      </span>
+      <Button label="Save" class="mt-2" @click="savePlan"/>
     </Dialog>
   </main>
 </template>
+
+<style scoped lang="scss">
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #f8f9fa;
+    padding: 8px;
+    border-bottom: 1px solid #dee2e6;
+
+    h1 {
+      font-size: 16px;
+      font-weight: 600;
+      margin-bottom: 0;
+    }
+
+    .edit-plan-btn {
+      padding: 4px 8px;
+      background-color: #007bff;
+      font-family: Noto Sans,sans-serif;
+    }
+  }
+  .form-label {
+    font-size: 12px;
+    margin-bottom: 0;
+  }
+
+  .query-plan-text-area {
+    font-family: monospace;
+    height: 30vh;
+  }
+
+  .query-text-area {
+    font-family: monospace;
+    height: 10vh;
+  }
+
+</style>
