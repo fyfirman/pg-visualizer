3f626a288bc7ac99df182831de8a3a7af9a8bd0d
Firmansyah Yanuar <fyfirman@gmail.com>
2023-05-11 23:20:29 +0700

    feat: add dialog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import PevVue from '../components/Pev.vue';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
 
+const visible = ref(false);
+
+
+function toggleVisible() {
+  visible.value = !visible.value;
+}
 </script>
 
 <template>
   <main>
+    <Button label="Show" @click="toggleVisible" />
     <PevVue />
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Edit Plan"
+      :style="{ width: '80vw' }"
+    >
+      <p>Edit query modal</p>
+    </Dialog>
   </main>
 </template>
